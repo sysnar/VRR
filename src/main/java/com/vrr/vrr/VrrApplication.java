083ef87b281525d8bfeb65df72560456a9305239
sysnar <sysn4r@gmail.com>
2022-05-31 19:53:10 +0900

    init: 프로젝트 세팅 [NO-ISSUE]

---
+++ src/main/java/com/vrr/vrr/VrrApplication.java
@@ -0,0 +1,13 @@
+package com.vrr.vrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vrr/vrr/VrrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrr.vrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
