5b4a2bb010654a6d1624cbd0e9eeb534acbf4815
sysnar <sysn4r@gmail.com>
2022-06-07 21:28:01 +0900

    build: 프로젝트 멀티모듈 세팅 [NO-ISSUE]

--- application/api/src/main/java/com/vrr/vrr/ApiApplication.java
+++ application/api/src/main/java/com/vrr/vrr/ApiApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VrrApplication {
+public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VrrApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class);
+    }
 }
